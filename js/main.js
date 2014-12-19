471a24b6d11014ec1ecd48ec339caf66bd41c9a0
Tharon Carlson <design@tharoncarlson.com>
2014-12-19 12:03:56 -0700

    fixed main.js to work with contactform7 nested fields

--- js/main.js
+++ js/main.js
@@ -1,27 +1,1 @@
-(function ($, root, undefined) {
-    $(function () {
-    'use strict';
-    //Hide the field initially
-        $('.child-opt').hide();
-        console.log('test1');
-
-    //Selects the dropdown when it it changes
-        $(".parent-opt").change(function() {
-            console.log('test2');
-
-    //Get the value of the opt selected
-            var select = $(this).val();
-            console.log(select);
-
-    //Hides all children on change 
-            $(this).siblings(".child-opt" ).hide(600);
-
-    //Shows the child opt that matches parent opt selected        
-            $(this).siblings(".child-opt" ).each( function( index, element ){
-                if ($(this).data('hide-opt') == select) {
-                    $(this).show(600);
-                }
-            });
-        });
-    });
-})(jQuery, this);
+(function ($, root, undefined) {    $(function () {    'use strict';    //Hide the field initially        $('.child-opt').hide();    //Selects the dropdown when it it changes        $(".parent-opt").change(function() {    //Get the value of the opt selected            var select = $(this).val();            console.log(select);    //Hides all children on change            $(this).parent().siblings(".child-opt" ).hide(600);    //Shows the child opt that matches parent opt selected                    $(this).parent().siblings(".child-opt" ).each( function( index, element ){                if ($(this).data('hide-opt') == select) {                    $(this).show(600);                }            });        });    });})(jQuery, this);
